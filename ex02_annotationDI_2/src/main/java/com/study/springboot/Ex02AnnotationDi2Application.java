a9b86814738a02a52fb89c2f406e0a2fa616313a
ohayo <qkzkel17@naver.com>
2024-08-05 18:29:37 +0900

    20240805

---
+++ ex02_annotationDI_2/src/main/java/com/study/springboot/Ex02AnnotationDi2Application.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex02AnnotationDi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex02AnnotationDi2Application.class, args);
+	}
+
+}
